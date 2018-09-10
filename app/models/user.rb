6cfe5936d91cbf4162c0fa13a328d999e7163113
DhanshreeK <dhanshree.j.kamble@gmail.com>
2018-09-10 18:14:23 +0530

    initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :token_authenticatable, :confirmable, :database_authenticatable
+	before_save :ensure_authentication_token
+
+	def skip_confirmation!
+	  self.confirmed_at = Time.now
+	end
+end
